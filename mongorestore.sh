110112ea6fc1506bb7f602036ecaaa4db3a15f60
betuah <betuahanugerah@gmail.com>
2022-02-17 15:38:27 +0700

    git update mongorestore.sh

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,2 +1,3 @@
-mongorestore -d lms /data/backup/lms
-mongorestore -d log_lms /data/backup/lms_log
+#!/bin/sh
+mongorestore -u $MONGO_USER -p $MONGO_PASSWORD -d lms /data/backup/lms --authenticationDatabase admin
+mongorestore -u $MONGO_USER -p $MONGO_PASSWORD -d log_lms /data/backup/log_lms --authenticationDatabase admin
