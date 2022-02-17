157da5c3ae6bfdb36355cdcbc2223f3986d3cc19
betuah <betuahanugerah@gmail.com>
2022-02-17 12:50:35 +0700

    change mongodb connection

--- src/setting/connection.php
+++ src/setting/connection.php
@@ -3,8 +3,12 @@
 try
 {
 //	$conn	= new MongoClient('127.0.0.1:27017');
-	$conn	= new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@127.0.0.1:27017");
+//	$conn	= new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@127.0.0.1:27017");
 //	$conn   = new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@10.10.10.25:27017");
+	$conn   = new MongoDB\Client(
+		'mongodb+srv://betuah:br4v0s34m0l3c@localhost/lms?retryWrites=true&w=majority'
+	);
+
 	$db		= $conn->lms;
 }
 catch(Exception $e)
