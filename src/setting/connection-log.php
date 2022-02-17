99a807864b75938543e0493414d08720ad5add57
betuah <betuahanugerah@gmail.com>
2022-02-17 13:02:29 +0700

    Update connection log

--- src/setting/connection-log.php
+++ src/setting/connection-log.php
@@ -5,8 +5,12 @@ try
 	// $connLog	= new MongoClient('127.0.0.1:27017');
 	// $connLog	= new MongoClient("mongodb://myppdb:s3rv3rppdb4dm1nj4b4r@10.10.10.13:27017");
 	// $connLog  	= new MongoClient('mongodb://10.10.10.13', array('username'=>'loglms','password'=>'s1m0l3kdb'));
-         $connLog       = new MongoClient('mongodb://127.0.0.1', array('username'=>'log_lms','password'=>'s3t4r@Log_lms'));
+    // $connLog       = new MongoClient('mongodb://127.0.0.1', array('username'=>'log_lms','password'=>'s3t4r@Log_lms'));
 	//$connLog  	= new MongoClient('mongodb://103.52.145.197:8888', array('username'=>'loglms','password'=>'s1m0l3kdb'));
+	$connLog   = new MongoDB\Driver\Manager(
+		'mongodb+srv://betuah:br4v0s34m0l3c@localhost/log_lms?retryWrites=true&w=majority'
+	);
+	
 	$dbLog		= $connLog->log_lms;
 }
 catch(Exception $e)
