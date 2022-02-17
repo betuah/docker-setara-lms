9d0b2810332991cfe3b11709602b47b8a27cea76
betuah <betuahanugerah@gmail.com>
2022-02-17 12:59:01 +0700

    update mongodb connection

--- src/setting/connection.php
+++ src/setting/connection.php
@@ -5,7 +5,7 @@ try
 //	$conn	= new MongoClient('127.0.0.1:27017');
 //	$conn	= new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@127.0.0.1:27017");
 //	$conn   = new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@10.10.10.25:27017");
-	$conn   = new MongoDB\Client(
+	$conn   = new MongoDB\Driver\Manager(
 		'mongodb+srv://betuah:br4v0s34m0l3c@localhost/lms?retryWrites=true&w=majority'
 	);
 
