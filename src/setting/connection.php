fb640825afd80864a4105caec3a8ce0a2056aa41
betuah <betuahanugerah@gmail.com>
2022-02-18 14:48:07 +0700

    Add new class mongoclient

--- src/setting/connection.php
+++ src/setting/connection.php
@@ -5,6 +5,8 @@ try
 //	$conn	= new MongoClient('127.0.0.1:27017');
 //	$conn	= new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@127.0.0.1:27017");
 //	$conn   = new MongoClient("mongodb://mylms:s3rv3rppdb4dm1nj4b4r@10.10.10.25:27017");
+// 	$conn   = new \MongoClient("mongodb://betuah:br4v0s34m0l3c@mongodb:27017");
+
 	$conn   = new MongoDB\Driver\Manager(
 		'mongodb+srv://betuah:br4v0s34m0l3c@localhost/lms?retryWrites=true&w=majority'
 	);
